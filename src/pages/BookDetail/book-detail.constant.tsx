5b0f4eb3def2ad636a747dccf0cd45f231191677
Thinh Tran <thinh.tran@banvien.com.vn>
2019-07-03 16:20:07 +0700

    Update animation for book detail

--- src/pages/BookDetail/book-detail.constant.tsx
+++ src/pages/BookDetail/book-detail.constant.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { Header } from 'react-navigation'
 
 export const styles = StyleSheet.create({
   container: {
@@ -16,21 +17,44 @@ export const styles = StyleSheet.create({
   bookCover: {
     width: 300,
     height: 300,
-    marginTop: 200
+    marginTop: Header.HEIGHT + 52
   },
   backgroundCover: {
     position: 'absolute',
     top: 0,
     zIndex: 1,
-    left: '-30%',
-    width: '160%',
+    left: '-25%',
+    width: '150%',
     height: '50%',
     right: 0,
-    opacity: 0.5,
     overflow: 'hidden'
   },
   backgroundCoverImg: {
-    width: '100%',
-    height: '100%'
+    // width: '100%',
+    // height: '100%'
+  },
+  viewWrapper: {
+    height: Header.HEIGHT,
+    flexDirection: 'row'
+  },
+  backButtonWrapper: {
+    justifyContent: 'center',
+    flex: 0.3
+  },
+  backButton: {
+    borderWidth: 0,
+    alignSelf: 'flex-start',
+    backgroundColor: 'transparent'
+  },
+  backButtonIcon: { color: '#fff' },
+  titleWrapper: {
+    justifyContent: 'center',
+    flex: 0.4
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#fff'
   }
 })
--- src/pages/BookDetail/book-detail.tsx
+++ src/pages/BookDetail/book-detail.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Animated, Image, ImageBackground, StatusBar, View } from 'react-native'
-import { NavigationScreenProps, ScrollView } from 'react-navigation'
+import { Animated, StatusBar, Text, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import { Header, NavigationScreenProps, ScrollView } from 'react-navigation'
+
+import { Button, Icon } from '@ant-design/react-native'
 import { styles } from './book-detail.constant'
+
 const HEADER_MAX_HEIGHT = 300
-const HEADER_MIN_HEIGHT = 0
+const HEADER_MIN_HEIGHT = Header.HEIGHT + (StatusBar.currentHeight || 0)
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT
 
 interface Props extends NavigationScreenProps<any> {}
@@ -21,9 +25,14 @@ export default class BookDetail extends React.Component<Props, State> {
   }
 
   render() {
-    const headerHeight = this.scrollY.interpolate({
-      inputRange: [0, HEADER_SCROLL_DISTANCE],
+    const headerHeightExtended = this.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE * 1.25],
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+    const opacity = this.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [1, 0],
       extrapolate: 'clamp'
     })
     return (
@@ -31,20 +40,19 @@ export default class BookDetail extends React.Component<Props, State> {
         <StatusBar translucent={true} backgroundColor="transparent" />
         <View style={styles.contentWrapper}>
           <ScrollView
-            bounces={false}
-            showsHorizontalScrollIndicator={false}
+            overScrollMode="always"
             scrollEventThrottle={16}
             onScroll={Animated.event([
               { nativeEvent: { contentOffset: { y: this.scrollY } } }
             ])}
             contentContainerStyle={styles.contentContainerStyle}
           >
-            <Image
+            <FastImage
               style={styles.bookCover}
               resizeMode="contain"
               source={{
                 uri:
-                  'http://www.creativindie.com/wp-content/uploads/2013/10/Enchantment-Book-Cover-Best-Seller1.jpg'
+                  'https://editorial01.shutterstock.com/wm-preview-1500/10104654b/006d1b09/alita-battle-angel-film-2019-shutterstock-editorial-10104654b.jpg'
               }}
             />
             <View>
@@ -57,21 +65,71 @@ export default class BookDetail extends React.Component<Props, State> {
             </View>
           </ScrollView>
         </View>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: 9
+          }}
+        >
+          <View style={{ height: StatusBar.currentHeight }} />
+          <View
+            style={{
+              flex: 1,
+              height: Header.HEIGHT,
+              justifyContent: 'center'
+            }}
+          >
+            <View style={styles.viewWrapper}>
+              <View style={styles.backButtonWrapper}>
+                <Button
+                  activeStyle={{ backgroundColor: 'transparent' }}
+                  style={styles.backButton}
+                >
+                  <Icon name="left" style={styles.backButtonIcon} />
+                </Button>
+              </View>
+              <View style={styles.titleWrapper}>
+                <Text style={styles.title}>Book Info</Text>
+              </View>
+            </View>
+          </View>
+        </View>
         <Animated.View
           style={{
             ...styles.backgroundCover,
-            borderBottomLeftRadius: headerHeight,
-            borderBottomRightRadius: headerHeight
+            borderBottomLeftRadius: headerHeightExtended,
+            borderBottomRightRadius: headerHeightExtended,
+            height: headerHeightExtended
           }}
         >
-          <ImageBackground
-            style={styles.backgroundCoverImg}
-            resizeMode="cover"
-            source={{
-              uri:
-                'http://www.creativindie.com/wp-content/uploads/2013/10/Enchantment-Book-Cover-Best-Seller1.jpg'
+          <Animated.View
+            style={{
+              zIndex: 0,
+              height: '100%',
+              position: 'absolute',
+              backgroundColor: '#F23F3C',
+              width: '100%'
             }}
           />
+          <Animated.View style={{ opacity, flex: 1 }}>
+            <FastImage
+              style={{
+                ...styles.backgroundCoverImg,
+                zIndex: 1,
+                top: 0,
+                left: 0,
+                flex: 1
+              }}
+              resizeMode="cover"
+              source={{
+                uri:
+                  'https://editorial01.shutterstock.com/wm-preview-1500/10104654b/006d1b09/alita-battle-angel-film-2019-shutterstock-editorial-10104654b.jpg'
+              }}
+            />
+          </Animated.View>
         </Animated.View>
       </View>
     )
